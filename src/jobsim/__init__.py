5617e0ef193574ef800db709cc3fb90ef26e4f74
adi sapir <adi.sapir5@gmail.com>
2025-09-25 14:10:30 +0300

    Update package configuration for pip install
    - Bump version to 0.1.4
    - Add command line entry points for all tools:
      - sim-histogram: histogram visualization with --file and --bins options
      - job-scheduling-view: job scheduling visualization with --file, --step, and --debug options
    - Update __init__.py to export all new classes:
      - SimHistogramBin, SimHistogramStacked
      - JobSchedulingView, JobSchedulingStep
    - Package now installs all command line tools via pip install

--- src/jobsim/__init__.py
+++ src/jobsim/__init__.py
@@ -2,7 +2,7 @@
 JobSim - A discrete event simulation system for job execution
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = "Adi Sapir"
 __email__ = "adi.sapir5@gmail.com"
 
@@ -11,7 +11,8 @@ from .jobgen import JobGenerator, Job
 from .workers_model import WorkerPool, WorkerStatus, Worker, POOL_PROPERTIES
 from .sim_config import SimulationConfig, CONFIG, load_config
 from .event_queue import EventQueue, Event
-from .sim_histogram import SimHistogram
+from .sim_histogram import SimHistogram, SimHistogramBin, SimHistogramStacked
+from .job_secheduling_view import JobSchedulingView, JobSchedulingStep
 from .time_def import MINUTE, HOUR, DAY, seconds_to_hms, parse_duration_hms, seconds_to_hms_short
 from .debug_config import debug_print, trace_print, full_debug_print, set_debug, get_debug, is_debug_enabled, is_trace_enabled, is_full_debug_enabled
 
@@ -29,11 +30,16 @@ __all__ = [
     "EventQueue",
     "Event",
     "SimHistogram",
+    "SimHistogramBin",
+    "SimHistogramStacked",
+    "JobSchedulingView",
+    "JobSchedulingStep",
     "MINUTE",
     "HOUR",
     "DAY",
     "seconds_to_hms",
     "parse_duration_hms",
+    "seconds_to_hms_short",
     "debug_print",
     "trace_print",
     "full_debug_print",
